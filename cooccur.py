f67ab5091a03beb82a3c81ed24e8428092f6f60c
jc2528 <jc2528@cornell.edu>
2019-04-11 09:28:46 -0400

    svd stuff

--- cooccur.py
+++ cooccur.py
@@ -8,26 +8,25 @@ with open('Data/title_to_index.pickle', 'rb') as f:
 	title_to_ind = pickle.load(f)
 # print(title_to_ind)
 
-# print(all_data['Dried Fruit Kiwi-Strawberry Mix'])
+"""Takes in our dictionary of snack data and returns a matrix where element [i,j]
+means that product j was also bought with product i"""
 def create_term_doc_mat(data):
-	term_doc_mat = np.zeros((len(data), len(data)))
+	term_doc_mat = np.ones((len(data), len(data)))
 	for key, data_dict in data.items():
 		curr_snack_ind = title_to_ind[key]
 		related_snacks = data_dict['also_bought']
-		# print(related_snacks)
-		# print(related_snacks)
 		related_snacks_ind = [title_to_ind[x] for x in related_snacks]
 		for i in related_snacks_ind:
-			# print("BEFORE" + str(term_doc_mat[curr_snack_ind, i]))
 			term_doc_mat[curr_snack_ind, i] += 1
-			# print("AFTER" + str(term_doc_mat[curr_snack_ind, related_snacks_ind]))
 	return term_doc_mat
 
 def create_cooccurence_mat(term_doc_mat):
-	pass
+	return np.dot(term_doc_mat.T, term_doc_mat)
 
-def PMI(cooccurence_mat):
-	pass
+def PMI(cooccurence_mat, term_doc_mat):
+	sum_term_doc = np.sum(term_doc_mat, axis = 0)
+	PMI_1 = np.divide(cooccurence_mat, sum_term_doc)
+	return np.divide(PMI_1.transpose(), sum_term_doc)
 
 def pmi_sim(snack_title, pmi_mat):
 	pass
@@ -36,6 +35,9 @@ def pmi_sim(snack_title, pmi_mat):
 # 	pass
 
 term_doc_mat = create_term_doc_mat(all_data)
+cooccur_mat = create_cooccurence_mat(term_doc_mat)
+pmi = PMI(cooccur_mat, term_doc_mat)
+print(pmi)
 # test_ind = title_to_ind['Frito Lay Sabritas Japanese Style Peanuts']
 # print(all_data['Frito Lay Sabritas Japanese Style Peanuts'])
 # print(np.sum(term_doc_mat[test_ind, :]))
